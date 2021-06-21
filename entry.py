b13b1443d86c704166a162383735cea6e014b0d1
ImHereByChance <kadermetov@gmail..com>
2021-06-21 14:44:55 +0300

    console args and reload mode added

--- entry.py
+++ entry.py
@@ -1,8 +1,32 @@
 import aiohttp
+import argparse
+import aioreloader
+import asyncio
 from source import create_app
 from source.routes import setup_routes
 
-if __name__ == '__main__':
-    app = create_app()
 
-    aiohttp.web.run_app(app,)
+app = create_app()
+
+
+# console arguments
+arg_parser = argparse.ArgumentParser(description='App launcher parser')
+arg_parser.add_argument('--host', help='Host to listen', default='127.0.0.1')
+arg_parser.add_argument(
+    '--port', 
+    help='Port to accept connections',
+    default=5000)
+arg_parser.add_argument(
+    '--reload',
+    action='store_true',
+    help='Autoreload code on change')
+args = arg_parser.parse_args()
+
+# reload mode
+if args.reload:
+    print('Start with reload mode')
+    aioreloader.start()
+
+
+if __name__ == '__main__':
+    aiohttp.web.run_app(app, host=args.host, port=args.port)
